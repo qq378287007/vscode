4c2a285dc3f0b00743c7b0c329b408011dfb605d
qq378287007 <qq378287007@163.com>
2023-07-10 17:35:01 +0800

    c/cpp 调试

---
+++ c_cpp/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
+
+/*
+Ctrl+Shift+P(查看->命令面板)
+c/c++
+*/
+
+/*
+Ctrl+Shift+P(查看->命令面板)
+Tasks:Configure Default Build Task
+C/C++: g++.exe build active file
+*/
+
+/*
+运行->添加配置
+C++(GDB/LLDB)
+*/
